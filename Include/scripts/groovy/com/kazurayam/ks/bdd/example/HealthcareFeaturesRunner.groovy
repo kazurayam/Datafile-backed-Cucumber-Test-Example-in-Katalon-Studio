88bb333e0a3bd473f5b10a44fb36de4898678e97
kazurayam <kazuaki.urayama@gmail.com>
2021-05-03 10:10:11 +0900

    save updates

--- Include/scripts/groovy/com/kazurayam/ks/bdd/example/HealthcareFeaturesRunner.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/bdd/example/HealthcareFeaturesRunner.groovy
@@ -5,11 +5,15 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+import com.kms.katalon.core.configuration.RunConfiguration
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features="Include/features/healthcare",
 // glue="",           // glue is defined by "Test Listeners/HealthcareCucuberTestGLUE.groovy"
 plugin = [	"pretty",
 	"junit:CucumberReports/healthcare/cucumber.xml",
 	"html:CucumberReports/healthcare",
-	"json:CucumberReports/healthcare/cucumber.json"])
+	"json:CucumberReports/healthcare/cucumber.json"
+	
+])
 public class HealthcareFeaturesRunner {}
